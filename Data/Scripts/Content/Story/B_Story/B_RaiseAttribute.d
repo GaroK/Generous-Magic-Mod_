7fab59aca6dd09ecb20de326b74cc3fdd8d852da
GaroK <raafalczekala@gmail.com>
2024-10-01 14:42:11 +0200

    Independent mana training
    Tried making bonus independent mana training.

--- Data/Scripts/Content/Story/B_Story/B_RaiseAttribute.d
+++ Data/Scripts/Content/Story/B_Story/B_RaiseAttribute.d
@@ -42,7 +42,7 @@ func void B_RaiseAttribute (var C_NPC oth, var int attrib, var int points)
 	
 	// ------ MANA_MAX steigern ------
 	if (attrib == ATR_MANA_MAX)			
-	{	
+	{	S
 		oth.attribute[ATR_MANA_MAX] = oth.attribute[ATR_MANA_MAX] + points;
 
 		concatText = ConcatStrings(PRINT_LearnMANA_MAX, IntToString(points));
@@ -58,5 +58,5 @@ func void B_RaiseAttribute (var C_NPC oth, var int attrib, var int points)
 		PrintScreen	(concatText, -1, -1, FONT_SCREEN, 2);
 	};
 	
-	B_RaiseRealAttributeLearnCounter (oth, attrib, points);
+	
 };
