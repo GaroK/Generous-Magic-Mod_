c09ae4c68040dbd42be5bba635ca98a70d87c2fc
GaroK <30365395+GaroK@users.noreply.github.com>
2026-01-13 15:24:15 +0100

    Add new functions to externals.d

--- .ci/externals.d
+++ .ci/externals.d
@@ -1,4 +1,7 @@
 // #if __gothic_dat__ { // This is new dacode version directive
 #if _gothic_dat {
     func instance AI_StartTriggerScript(var string f, var int delay) {};
+    func void AI_CallScript(var string funcName, var C_NPC slf, var C_NPC oth) {};
+    func void Hlp_PrintConsole(var string message) {};
+    func string Str_Format( var string format, ... ) {};
 }
