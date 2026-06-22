6f7a13e9d648a22c772a420895765e6a23a09ada
GaroK <raafalczekala@gmail.com>
2026-06-22 15:55:57 +0200

    RPD Update
    "So, I decided to reorganize my plugin's structure to make it much and much more correct and convinient. Your FX is now placed to the \UCS\FX folder. I packed the sources. just unpack it to your \autorun (don't forget to delete or deactivate previous ones). Btw, you need to have a Gothic.src script in \autorun to let parser do its job correctly. If you have one already , then just append it with my file strings. just put them below others. And that's it. All afterburnFX config, as I said, is in UCS\FX folder now. (area radius and area inclusion for pyrokinesis too). It's unbelievably flexible nowadays I tell u! 🫨"

---
+++ .ci/.release/System/Autorun/UCS/FX/FXProto/Init.d
@@ -0,0 +1,13 @@
+
+func int ff(){ return false; }; // false exit/include condition
+func int tf(){ return true; }; // true exit/include condition
+
+func void UCS_InitFXProto()
+{
+	// FX PROTOTYPES MUST BE REGISTERED
+	
+	/* InitPoisonFXP(); */
+	/* InitLightningFXP(); */
+	/* InitSomeFXP(); */
+	/* InitAnotherFXP(); */
+};
